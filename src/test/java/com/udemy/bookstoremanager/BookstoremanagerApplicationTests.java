5b0a0e23dd5add35e270baf4622f8cc6075e9e1b
Sandro Roberto Ventura <sandrorv88@gmail.com>
2022-12-01 17:25:14 -0300

    First commit: project structure created

---
+++ src/test/java/com/udemy/bookstoremanager/BookstoremanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemy.bookstoremanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoremanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
